f7f2f07a4a0f3b2766bda7475fad36f41aeb0146
Deni Prasetyo <deni.prasetyo@solecode.id>
2025-08-04 18:31:24 +0700

    update sidebarr

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -167,7 +167,7 @@
                     class="flex items-center justify-center rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                     <span class="ml-2">
                         <i class="fa-solid fa-arrow-right-from-bracket w-5 text-white group-hover:text-white"></i>
-                        Logout
+                        Keluar
                     </span>
                 </a>
             </div>
